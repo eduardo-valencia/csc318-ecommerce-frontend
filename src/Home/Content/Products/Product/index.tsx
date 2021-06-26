4d4a52173d50eeb48cb4aa9ee0d4be91390f92f6
Eduardo Valencia <8025918@gmail.com>
2021-06-26 12:44:02 -0700

    Add product image

--- src/Home/Content/Products/Product/index.tsx
+++ src/Home/Content/Products/Product/index.tsx
@@ -11,21 +11,22 @@ import Product from '../../../../api/types/Product'
 import { getProductLink } from '../../../../utils/links'
 import { formatPrice } from '../../../../utils/price'
 import QuantityButtons from './QuantityButtons'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const styles = () => {
   const paddingX: string = '0.875rem'
+  const borderRadius = '0.625rem'
   return createStyles({
     root: {
       listStyle: 'none',
+      width: 'calc(50% - 0.625rem / 2)',
     },
     link: {
-      textDecoration: 'none',
-      backgroundColor: 'white',
-      position: 'relative',
-      borderRadius: '0.625rem',
       boxShadow: '#00000029 0 0.1875rem 0.375rem',
+      borderRadius,
+      textDecoration: 'none',
+      position: 'relative',
       marginBottom: '0.625rem',
-      flexBasis: 'calc(50% - 0.625rem / 2)',
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'space-between',
@@ -44,23 +45,41 @@ const styles = () => {
       display: 'flex',
       alignItems: 'flex-end',
       justifyContent: 'space-between',
+      paddingLeft: paddingX,
     },
     name: {
       textTransform: 'uppercase',
       fontSize: '0.875rem',
       color: '#304A4E',
+      marginRight: '0.25rem',
+      marginBottom: '0.6875rem',
+    },
+    image: {
+      borderRadius,
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      zIndex: -1,
     },
   })
 }
 
 interface Props extends WithStyles<typeof styles>, Product {}
 
-const ProductItem = ({ classes, price, name, slug }: Props) => {
+const ProductItem = ({ classes, price, name, slug, thumbnail }: Props) => {
   const href: string = getProductLink(slug)
   const formattedPrice: string = formatPrice(price)
+  const image = getImage(thumbnail.localFile)
   return (
     <li className={classes.root}>
       <Link to={href} className={classes.link}>
+        <GatsbyImage
+          image={image}
+          alt={thumbnail.alternativeText}
+          className={classes.image}
+        />
         <div className={classes.top}>
           <Typography className={classes.price}>{formattedPrice}</Typography>
         </div>
