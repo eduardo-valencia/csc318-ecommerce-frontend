98be1ab4d2aecceff64f8658ae8ff076a0d3e4c2
Eduardo Valencia <8025918@gmail.com>
2021-07-09 13:48:45 -0700

    Hide products and checkout when no items

--- src/Cart/index.tsx
+++ src/Cart/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {
   withStyles,
   WithStyles,
   createStyles,
   Container,
+  Typography,
 } from '@material-ui/core'
 import { Helmet } from 'react-helmet'
 
@@ -11,6 +12,7 @@ import Layout from '../components/Layout'
 import Nav from './Nav'
 import Products from './Products'
 import Checkout from './Checkout'
+import { CartContext } from '../contexts/CartContext'
 
 const styles = () => {
   return createStyles({
@@ -23,14 +25,29 @@ const styles = () => {
 interface Props extends WithStyles<typeof styles> {}
 
 const Cart = ({ classes }: Props) => {
+  const { cart } = useContext(CartContext)!
+
+  const renderProductsAndCheckout = () => {
+    if (cart.length) {
+      return (
+        <>
+          <Products />
+          <Checkout />
+        </>
+      )
+    }
+    return <Typography>You have no items in your cart.</Typography>
+  }
+
+  const productsAndCheckout: JSX.Element = renderProductsAndCheckout()
+
   return (
     <Layout nav={<Nav />}>
       <Container className={classes.container}>
         <Helmet>
           <title>Cart</title>
         </Helmet>
-        <Products />
-        <Checkout />
+        {productsAndCheckout}
       </Container>
     </Layout>
   )
