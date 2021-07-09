21a4bf957c6c2250c95569633925c0000b97fd1d
Eduardo Valencia <8025918@gmail.com>
2021-07-09 12:46:04 -0700

    Replace button colors

--- src/Cart/Checkout.tsx
+++ src/Cart/Checkout.tsx
@@ -12,6 +12,8 @@ const styles = () => {
       lineHeight: '1.25rem',
       left: 0,
       right: 0,
+      backgroundColor: '#94A8B3',
+      color: 'white',
     },
   })
 }
@@ -20,7 +22,7 @@ interface Props extends WithStyles<typeof styles> {}
 
 const Checkout = ({ classes }: Props) => {
   return (
-    <Button variant='contained' color='primary' classes={classes} fullWidth>
+    <Button variant='contained' classes={classes} fullWidth>
       Checkout
     </Button>
   )
